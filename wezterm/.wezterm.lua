7bb16fcf640edda6c79fa58ce883dc276bf495a8
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-07-30 09:58:31 +0000

    feat: added .zhrc files and wezterm files

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Macchiato'
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 9.5
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+
+-- and finally, return the configuration to wezterm
+return config
