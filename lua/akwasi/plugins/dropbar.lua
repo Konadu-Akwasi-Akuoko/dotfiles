a7933f8e55b5986a486bc432a3017b2f98bdfe3e
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-07-31 11:55:15 +0000

    feat: added fuzzy finding to the dropbar

--- lua/akwasi/plugins/dropbar.lua
+++ lua/akwasi/plugins/dropbar.lua
@@ -4,4 +4,17 @@ return {
 	dependencies = {
 		"nvim-telescope/telescope-fzf-native.nvim",
 	},
+	config = function()
+		require("dropbar")
+
+		vim.g.mapleader = " "
+		local keymap = vim.keymap
+
+		function open_fuzzy_finder()
+			require("dropbar.api").fuzzy_find_toggle()
+		end
+
+		-- Map a key to open fuzzy finder mode, when drobar is toggled
+		keymap.set("n", "f", ":lua open_fuzzy_finder()<CR>", { noremap = true, silent = true })
+	end,
 }
