e52ecfb4175f14b40dd1a3da1ec712d87df1dfec
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2024-12-31 22:05:22 +0000

    feat: updated catpuccin

--- lua/akwasi/plugins/bufferline.lua
+++ lua/akwasi/plugins/bufferline.lua
@@ -1,11 +1,18 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			separator_style = "slope",
+			mode = "buffers",
+
+			diagnostics = "nvim_lsp",
+			--- Don't get too fancy as this function will be executed a lot
+			diagnostics_indicator = function(count, level)
+				local icon = level:match("error") and " " or " "
+				return " " .. icon .. count
+			end,
+		},
+	},
 }
