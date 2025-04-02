36205f92c412779e178acac0fac819cfdba72376
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2025-04-02 09:41:32 +0000

    feat: updated features to use 0.11 features

--- lua/akwasi/plugins/lsp/lspconfig.lua
+++ lua/akwasi/plugins/lsp/lspconfig.lua
@@ -82,12 +82,17 @@ return {
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			virtual_lines = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.HINT] = "󰠠 ",
+					[vim.diagnostic.severity.INFO] = " ",
+				},
+			},
+		})
 
 		local on_attach = function(client, bufnr)
 			vim.keymap.set(
